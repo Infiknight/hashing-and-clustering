8c79c0a367c892f7343ae785dbcf5e077c21c5f9
“strker” <stratos-6@hotmail.com>
2014-12-01 21:41:01 +0200

    K Medoids header

--- k-medoids.h
+++ k-medoids.h
@@ -1,6 +1,6 @@
 #include "update_Improved_PAM.h"
 
 
-int kmedoids(struct hamming_element * datasetArr, int size, int k, int numOfHashFunctions, int L, int claransSetFraction, int claransIterations);
+int kmedoids(struct hamming_element * datasetArr, int size, int k, int numOfHashFunctions, int L, int claransSetFraction, int claransIterations, FILE* fpOutput, int complete);
 
 int stoppingCondition(struct hamming_element * datasetArr, int *medoids, int *newMedoids, int k);
