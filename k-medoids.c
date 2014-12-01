8a99f1b47729f2d167120642d1803937ff8ec71a
“strker” <stratos-6@hotmail.com>
2014-12-01 21:41:22 +0200

    K Medoids

--- k-medoids.c
+++ k-medoids.c
@@ -4,13 +4,19 @@
 #include "k-medoids.h"
 
 
-int kmedoids(struct hamming_element * datasetArr, int size, int k, int numOfHashFunctions, int L, int claransSetFraction, int claransIterations){
+int kmedoids(struct hamming_element * datasetArr, int size, int k, int numOfHashFunctions,
+			int L, int claransSetFraction, int claransIterations, FILE *fpOutput, int complete){
 
+	int i, j;
 	int * medoids, *newMedoids, *oldMedoids;
 	cluster * clusters;
+	struct timeval start, end;
 
 	oldMedoids = malloc(sizeof(int) * k);
 	newMedoids = malloc(sizeof(int) * k);
+
+
+	gettimeofday(&start, NULL);
 
 	medoids = hammingSpreadout(datasetArr,size , k);
 
@@ -20,11 +26,35 @@ int kmedoids(struct hamming_element * datasetArr, int size, int k, int numOfHash
 	oldMedoids = medoids;
 
 	while(stoppingCondition(datasetArr, oldMedoids, newMedoids, k)==0){
-
+		
 		clusters = hammingPAM(datasetArr, newMedoids, size, k);
-
+		
 		oldMedoids = newMedoids;
 		newMedoids = hammingUpdateAlaLloyd(clusters, datasetArr, size, k);
+
+	}
+
+	gettimeofday(&end, NULL);
+
+	printf("SIZE %d\n",clusters[0].size );
+
+	fprintf(fpOutput, "Algorithm: Ι1A1U1\n");
+
+	for(i=0; i<k; i++){
+		fprintf(fpOutput, "CLUSTER-%d {size: %d, medoid: item%d}\n", i+1, clusters[i].size, ((clusters[i].medoidIndex )+ 1) );
+	}
+	fprintf(fpOutput, "clustering_time: %f seconds\n", ((end.tv_sec * 1000000 + end.tv_usec)- (start.tv_sec * 1000000 + start.tv_usec))/1000000.00);
+
+	//Silhouette
+
+	if(complete == 1){
+		for(i=1; i<k; i++){
+			fprintf(fpOutput, "\nCLUSTER-%d {", i+1);
+			for(j=0; j<clusters[i].size; j++){
+				fprintf(fpOutput, "item%d, ", clusters[i].elementsIndexes[j]);
+			}
+			fprintf(fpOutput, "}\n");
+		}
 	}
 
 
